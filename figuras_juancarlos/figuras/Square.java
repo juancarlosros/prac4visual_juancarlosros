7554b832ec2a8e4abe7d12c37e6c0f770e195219
juancarlosros <157399453+juancarlosros@users.noreply.github.com>
2024-01-22 19:08:18 +0100

    Se añadió dos líneas

---
+++ figuras_juancarlos/figuras/Square.java
@@ -0,0 +1,14 @@
+// Square.java
+package figuras;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void calculateArea() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el lado del cuadrado: ");
+        double side = scanner.nextDouble();
+        double area = side * side;
+        System.out.println("El área del cuadrado es: " + area);
+    }
+}
